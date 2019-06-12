6d64b231d8c0d7afe877e7be6463cf011dda9777
Marcel Schneider <marcel@coopmasters.de>
2019-06-12 22:33:30 +0200

    cherry picked from https://github.com/ferkulat/csv2xls.git
    9dcad1d809bc172077407b1c0cfc62958e18f3d0

--- lib/fun.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by marcel on 3/31/17.
-//
-#include "funcomp.hpp"
-int fun(int a)
-{
-    return 2 *a;
-}
--- lib/funcomp.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by marcel on 3/31/17.
-//
-
-#ifndef CPPFUNCOMP_HPP
-#define CPPFUNCOMP_HPP
-int fun(int a);
-#endif //CPPFUNCOMP_HPP
