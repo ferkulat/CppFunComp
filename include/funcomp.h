ferkulat/csv2xls

added Do().If()

csv2xlslib/funcomp.h
@@ -30,49 +30,55 @@ namespace funcomp{
         }
         template <typename Callee> auto operator()(Callee callee) const
         {
-            while (true)
-            {
-                auto const result = callee();
-                if (m_pred(result))
-                    return result;
-            }
+            return [pred=m_pred, callee](auto &&... args) {
+                while (true)
+                {
+                    auto const result = callee(args...);
+                    if (pred(result))
+                        return result;
+                }
+            };
         }
     };
-    template<typename Callee, typename Pred>
-    auto DoIf(Callee callee, Pred pred)
+    template<typename Callee>
+    struct Do
     {
-        return [callee, pred](auto& value){
-            if (pred(value))
-            {
-                if constexpr (std::is_invocable_v<Callee,declval(value)>)
-                {
-                    return calee(value);
-                }
-                else
-                {
-                    return callee();
-                }
-            }
-            return value;
-        };
-    }
-
-    template<typename Callee, typename Caller>
-    auto operator|(Callee callee, Caller caller)
-    {
-        if constexpr (std::is_base_of_v<HigherOrderFunction, Caller>)
+        Callee callee;
+        constexpr Do(Callee callee_)
+                    :callee(callee_)
+                    {}
+        template<typename Pred>
+        constexpr auto If(Pred&& pred)
         {
-            return [callee, caller](){
-                return caller(callee);
+            return [callee_=callee, pred](auto && value){
+                if (pred(value))
+                {
+                    if constexpr (std::is_invocable_v<Callee,decltype(value)>)
+                    {
+                        return callee_(value);
+                    }
+                    else
+                    {
+                        return callee_();
+                    }
+                }
+                return value;
             };
+        }
+    };
+
+    template <typename Callee, typename Caller> auto operator|(Callee callee, Caller caller)
+    {
+        if constexpr (std::is_base_of_v<funcomp::HigherOrderFunction, Caller>)
+        {
+            return [hof = caller(callee)](auto&&... arg) { return hof(arg...); };
         }
         else
         {
-            return [callee, caller](auto const&... arg) { return caller(callee(arg...)); };
+            return [callee, caller](auto&&... arg) { return caller(callee(arg...)); };
         }
     }
 
-
-}
+    }
 
 #endif //CSV2XLS_FUNCOMP_H

ferkulat/CppFunComp

include/funcomp.h
@@ -30,49 +30,55 @@ namespace cppfuncomp{
         }
         template <typename Callee> auto operator()(Callee callee) const
         {
-            while (true)
-            {
-                auto const result = callee();
-                if (m_pred(result))
-                    return result;
-            }
+            return [pred=m_pred, callee](auto &&... args) {
+                while (true)
+                {
+                    auto const result = callee(args...);
+                    if (pred(result))
+                        return result;
+                }
+            };
         }
     };
-    template<typename Callee, typename Pred>
-    auto DoIf(Callee callee, Pred pred)
+    template<typename Callee>
+    struct Do
     {
-        return [callee, pred](auto& value){
-            if (pred(value))
-            {
-                if constexpr (std::is_invocable_v<Callee,declval(value)>)
-                {
-                    return calee(value);
-                }
-                else
-                {
-                    return callee();
-                }
-            }
-            return value;
-        };
-    }
-
-    template<typename Callee, typename Caller>
-    auto operator|(Callee callee, Caller caller)
-    {
-        if constexpr (std::is_base_of_v<HigherOrderFunction, Caller>)
+        Callee callee;
+        constexpr Do(Callee callee_)
+                    :callee(callee_)
+                    {}
+        template<typename Pred>
+        constexpr auto If(Pred&& pred)
         {
-            return [callee, caller](){
-                return caller(callee);
+            return [callee_=callee, pred](auto && value){
+                if (pred(value))
+                {
+                    if constexpr (std::is_invocable_v<Callee,decltype(value)>)
+                    {
+                        return callee_(value);
+                    }
+                    else
+                    {
+                        return callee_();
+                    }
+                }
+                return value;
             };
+        }
+    };
+
+    template <typename Callee, typename Caller> auto operator|(Callee callee, Caller caller)
+    {
+        if constexpr (std::is_base_of_v<cppfuncomp::HigherOrderFunction, Caller>)
+        {
+            return [hof = caller(callee)](auto&&... arg) { return hof(arg...); };
         }
         else
         {
-            return [callee, caller](auto const&... arg) { return caller(callee(arg...)); };
+            return [callee, caller](auto&&... arg) { return caller(callee(arg...)); };
         }
     }
 
-
-}
+    }
 
 #endif //CPPFUNCOMP_H